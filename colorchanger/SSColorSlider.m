a64fdbc8e03f79f7283d011e5292f10d4ec73601
David Douglas <dave@softwaresmoothie.com>
2011-07-22 22:53:45 -0500

    added licensing info

--- colorchanger/SSColorSlider.m
+++ colorchanger/SSColorSlider.m
@@ -1,10 +1,32 @@
 //
 //  SSColorSlider.m
-//  colorchanger
+//  Version 1.0
 //
 //  Created by David Douglas on 7/22/11.
 //  Copyright 2011 Software Smoothie. All rights reserved.
 //
+//  Licensing (Zlib)
+//  This software is provided 'as-is', without any express or implied
+//  warranty.  In no event will the authors be held liable for any damages
+//  arising from the use of this software.
+//
+//  Permission is granted to anyone to use this software for any purpose,
+//  including commercial applications, and to alter it and redistribute it
+//  freely, subject to the following restrictions:
+//
+//  1. The origin of this software must not be misrepresented; you must not
+//  claim that you wrote the original software. If you use this software
+//  in a product, an acknowledgment in the product documentation would be
+//  appreciated but is not required.
+//  2. Altered source versions must be plainly marked as such, and must not be
+//  misrepresented as being the original software.
+//  3. This notice may not be removed or altered from any source distribution.
+
+//  As a side note on using this code, you might consider giving some credit to me by
+//	1) linking my website from your app's website 
+//	2) or crediting me inside the app's credits page 
+//	3) or a tweet mentioning @davedoug1970
+//	
 
 #import "SSColorSlider.h"
 
